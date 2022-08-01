e17294dfb6711de0f12bc211b497f9ebf90b37c7
Nicolas Naconechney <nnaconech@ams.red>
2022-08-01 10:48:04 -0300

    creacion app, HTML, ignore, style

---
+++ app.js
@@ -0,0 +1,28 @@
+const express= require("express");
+const app = express();
+
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.listen(2022, ()=>console.log("Servidor MUUCS acitvo bebé!"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/index.html"))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+});
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/productCart.html"))
+});
+
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/productDetail.html"))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+});
