3332507eb78740fef1662980d7333fb8654abe9f
luuciooo <lucioabalos97@gmail.com>
2022-08-29 16:34:07 -0300

    renderizamos los partial con Nico

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,11 +1,11 @@
 const path = require('path');
 
 const controllers = {
-    index: (req, res) => res.render('index'),
-    login: (req, res) => res.render('login'),
-    productCart: (req, res) => res.render('productCart'),
-    productDetail: (req, res) => res.render('productDetail'),
-    register: (req, res) => res.render('register'),
+    index: (req, res) => res.render('index', {title: 'Home'}),
+    login: (req, res) => res.render('login',{title: 'Login'}),
+    productCart: (req, res) => res.render('productCart',{title: 'ProductCart'}),
+    productDetail: (req, res) => res.render('productDetail',{title: 'ProductDetail'}),
+    register: (req, res) => res.render('register',{title: 'Register'}),
 }
 
 module.exports = controllers;
