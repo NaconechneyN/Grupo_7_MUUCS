79c0d02864d473646af9ca0d328daacc1436865d
luuciooo <lucioabalos97@gmail.com>
2022-10-25 18:19:55 -0300

    arregle errores

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -9,26 +9,10 @@ const { body } = require('express-validator')
 //VER DONDE VA
 
 
-const validateUser = [
-    body('nombre')
-    .notEmpty().withMessage('Debes completar tu nombre y apellido'),
-    body('date')
-    .isDate().withMessage('Debes colocar tu fecha de nacimiento'),
-    body('email')
-    .isEmail().withMessage('Debes completar tu email'),
-    body('password')
-    .notEmpty().withMessage('Debes completar tu contraseña'),
-    body('password2')
-    .notEmpty().withMessage('Debes completar este campo'),
-    
-];
 
 const validateUser = require('../middlewares/mildwareUserForm');
 const validateLogin = require('../middlewares/mildwareUserLogin');
-<<<<<<< HEAD
-=======
 
->>>>>>> 142fcffd9ef0f0a53e4aadbc1e91c1056b779cc0
 
 // REQUIERO VISTA LOGIN
 
